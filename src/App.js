c45b7986408c8e2f1e5c80e609651c1e0916aafa
Nilesh Kawar <nilesh227kawar@gmail.com>
2021-11-21 21:11:47 +0530

    Video number 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+// import About from './components/About';
 
 function App() {
+  const [mode, setMode] = useState('light')
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About Us" homeText="Home"/>
+      <Navbar title="TextUtils" aboutText="About Us" homeText="Home" mode={mode}  />
       <TextForm heading="Enter the text to analyze"/>
+      {/* <About /> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,17 +11,42 @@ export default function TextForm(props) {
         setText(uppercase)
         // console.log("Upper Case was clicked");
     }
+    const handleLowerClick = () => {
+        let uppercase = text.toLowerCase();
+        setText(uppercase)
+    }
+    // const handleCapClick = () => {
+    //     let capLetters = [];
+    //     let words = text.split(" ")
+    //     for (let i = 0; i < words.length; i++) {
+    //         capLetters[i] =  words[i][0].toUpperCase() + words[i].substr(1);
+    //     }
+    //     let res = capLetters.join(" ")
+    //     setText(res)
+    //     console.log(res)
+    // }
     const handleOnChange = (event) => {
         setText(event.target.value)
-        // console.log("on chnge");
+        // conso le.log("on chnge");
     }
     return (
-        <div className="container">
-            <div className="mt-5 mb-2">
-                <h1 className="mb-3">{props.heading}</h1>
-                <textarea className="form-control" value={text} rows="8" onChange={handleOnChange}></textarea>
+        <>
+            <div className="container">
+                <div className="mt-5 mb-2">
+                    <h1 className="mb-3">{props.heading}</h1>
+                    <textarea className="form-control" value={text} rows="8" onChange={handleOnChange}></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UPPERCASE</button>
+                <button className="btn btn-primary mx-1" onClick={handleLowerClick}>Convert to lowercase</button>
+                {/* <button className="btn btn-primary" onClick={handleCapClick}>Convert to Capitalize</button> */}
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UPPERCASE</button>
-        </div>
+            <div className="container my-3">
+                <h1>Your text summary</h1>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
